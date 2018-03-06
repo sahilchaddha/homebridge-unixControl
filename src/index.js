a9d573b7ca153035e4df8b29ebe04f192a536dac
Sahil Chaddha <sahilchaddha@gmail.com>
2018-03-06 15:34:39 +0800

    Added url depedancy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 var Service, Characteristic, Accessory, hap, UUIDGen
 
 module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
 };
