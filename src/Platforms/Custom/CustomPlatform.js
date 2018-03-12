9e42e14cedc2d52536d3d322604e1ee26f9f5811
Sahil Chaddha <sahilchaddha@gmail.com>
2018-03-12 13:15:09 +0800

    Added CustomPlatform

---
+++ src/Platforms/Custom/CustomPlatform.js
@@ -0,0 +1,50 @@
+var container = require('../../Services/Container.js')
+const platformName = "CustomPlatform"
+
+var availableAccesories = {
+}
+
+function CustomPlatform(log, config, api) {
+    this.platformLogger = log
+    this.platformConfig = config
+    this.platformAPI = api
+    this.accessories = []
+    registerAccesories.call(this)  
+}
+
+CustomPlatform.prototype.configureAccessory = function(accessory) {
+    this.platformLogger.log('Configure Accessory')
+    var platform = this
+    accessory.reachable = true
+  
+    accessory.on('identify', function(paired, callback) {
+      platform.log(accessory.displayName, "Identify!!!")
+      callback();
+    })
+  
+    this.accessories.push(accessory)
+}
+
+CustomPlatform.prototype.configurationRequestHandler = function(context, request, callback) {
+
+}
+
+function registerAccesories() {
+    var platform = this
+    var accesoryKeys = Object.keys(availableAccesories)
+    var excludedAccesories = []
+
+    if (platform.platformConfig != null && platform.platformConfig["exclude"] != null && platform.platformConfig.exclude.length > 0) {
+        excludedAccesories = platform.platformConfig.exclude
+    }
+
+    accesoryKeys.forEach(key => {
+        if (excludedAccesories.indexOf(key) > -1) {
+            platform.accessories.push(availableAccesories[key])
+        }
+    })
+
+    platform.platformAPI.registerPlatformAccessories(container.pluginName, platformName, platform.accessories)
+}
+
+module.exports = CustomPlatform
--- src/Services/PlatformService.js
+++ src/Services/PlatformService.js
@@ -5,6 +5,7 @@ var wifiPlatform = require('../Platforms/Wifi/WifiPlatform.js')
 var bluetoothPlatform = require('../Platforms/Bluetooth/BluetoothPlatform.js')
 var systemSpyPlatform = require('../Platforms/SystemSpy/SystemSpyPlatform.js')
 var musicPlatform = require('../Platforms/Music/MusicPlatform.js')
+var customPlatform = require('../Platforms/Custom/CustomPlatform.js')
 var container = require('./Container.js')
 
 function PlatformService(homebridge) { 
@@ -21,6 +22,7 @@ PlatformService.prototype.addPlatforms = function(homebridge) {
     homebridge.registerPlatform(container.pluginName, "BluetoothPlatform", bluetoothPlatform, true)
     homebridge.registerPlatform(container.pluginName, "SystemSpyPlatform", systemSpyPlatform, true)
     homebridge.registerPlatform(container.pluginName, "MusicPlatform", musicPlatform, true)
+    homebridge.registerPlatform(container.pluginName, "CustomPlatform", true)
 }
 
 module.exports = PlatformService
